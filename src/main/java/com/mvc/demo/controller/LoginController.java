59536556f6cd7caef5032a5f1fc8ed50b224c5e6
Dawei <15554485117@163.com>
2018-12-03 22:54:57 +0800

    DEV: 页面问题解决

--- src/main/java/com/mvc/demo/controller/LoginController.java
+++ src/main/java/com/mvc/demo/controller/LoginController.java
@@ -12,10 +12,11 @@ import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Dawei 2018/11/17
@@ -25,11 +26,16 @@ public class LoginController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
+    /**
+     * 提交登陆信息
+     *
+     * @param userInfoDto 登陆用户信息体
+     */
     @PostMapping(value = "/subLogin")
-    public ModelAndView login(UserInfoDto userInfoDto) {
-        logger.info("登陆信息为 userInfo={}", JSON.toJSONString(userInfoDto));
+    public String login(UserInfoDto userInfoDto, Model model, String callbackUrl) {
+        logger.info("登陆信息为 userInfo={}, callbackUrl={}", JSON.toJSONString(userInfoDto), callbackUrl);
         ResultDto<String> resultDto = new ResultDto<>();
-        String resultUrl = "login";
+        String resultUrl = "/toLogin";
         if (userInfoDto == null || StringUtils.isEmpty(userInfoDto.getLoginName()) || StringUtils.isEmpty(userInfoDto.getPassWord()) || userInfoDto.getRememberType() == null) {
             resultDto.setParamError();
         } else {
@@ -39,7 +45,11 @@ public class LoginController {
             try {
                 subject.login(token);
                 resultDto.setSuccess();
-                resultUrl = "redirect: /indexPage";
+                if (!StringUtils.isEmpty(callbackUrl)) {
+                    resultUrl = "redirect:/" + callbackUrl;
+                } else {
+                    resultUrl = "redirect:/home/index";
+                }
             } catch (IncorrectCredentialsException e) {
                 resultDto.setResult(ErrorEnum.LOGIN_FAIL_PASSWORD);
                 resultDto.setData(e.getMessage());
@@ -52,10 +62,31 @@ public class LoginController {
             }
         }
         logger.info("登陆结果为： resultUrl={} resultDto={}", resultUrl, JSON.toJSONString(resultDto));
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName(resultUrl);
-        modelAndView.addObject("resultData", resultDto);
-        return modelAndView;
+        model.addAttribute("resultData", resultDto);
+        return resultUrl;
+    }
+
+
+    /**
+     * 請求空或根目录
+     */
+    @RequestMapping({"/", ""})
+    public String rootRequest() {
+        logger.info("进入Root请求~~~~~");
+        Subject subject = SecurityUtils.getSubject();
+        //已经认证跳转取去主页面 否则去登陆
+        return subject.isAuthenticated() ? "redirect:/home/index" : "/toLogin";
+    }
+
+    /**
+     * 进入首页
+     */
+    @GetMapping(value = "/home/index")
+    public String homePageIndex() {
+        logger.info("进入主页面请求~~~~~");
+        Subject subject = SecurityUtils.getSubject();
+        //已经认证跳转取去主页面 否则去登陆
+        return subject.isAuthenticated() ? "/homePageIndex" : "/toLogin";
     }
 
 }
